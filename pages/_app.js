04e55a134f652f244415b2c349e97602ba3607a0
bartosjiri <me@bartosjiri.com>
2022-02-18 22:53:34 +0100

    Finish project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import {Toaster} from 'react-hot-toast'
+
+import MetaMaskAccountProvider from '../components/meta-mask-account-provider'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return (
+    <MetaMaskAccountProvider>
+      <Toaster />
+      <Component {...pageProps} />
+    </MetaMaskAccountProvider>
+  )
 }
+
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,106 @@
+import {useState, useEffect} from "react";
+import {ethers} from "ethers";
+import {toast} from "react-hot-toast";
+
+import PrimaryButton from "../components/primary-button";
+import Keyboard from "../components/keyboard"
+import TipButton from "../components/tip-button";
+import {UserCircleIcon} from "@heroicons/react/solid"
+import {useMetaMaskAccount} from '../components/meta-mask-account-provider';
+
+import addressesEqual from "../utils/addressesEqual";
+
+import getKeyboardsContract from "../utils/getKeyboardsContract"
+
 export default function Home() {
-  return <p>The start of our app!</p>
+  const {ethereum, connectedAccount, connectAccount} = useMetaMaskAccount();
+
+  const keyboardsContract = getKeyboardsContract(ethereum);
+
+  const [keyboardsLoading, setKeyboardsLoading] = useState(false);
+  const [keyboards, setKeyboards] = useState([]);
+
+  async function getKeyboards() {
+    if (keyboardsContract && connectedAccount) {
+      setKeyboardsLoading(true);
+      try {
+        const keyboards = await keyboardsContract.getKeyboards();
+        console.log('Retrieved keyboards...', keyboards)
+
+        setKeyboards(keyboards)
+      } finally {
+        setKeyboardsLoading(false);
+      }
+    }
+  }
+
+  useEffect(() => {
+    getKeyboards();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [!!keyboardsContract, ethereum, connectedAccount]);
+
+  useEffect(() => {
+    if (keyboardsContract && connectedAccount) {
+      keyboardsContract.on('KeyboardCreated', async (keyboard) => {
+        if (connectedAccount && !addressesEqual(keyboard.owner, connectedAccount)) {
+          toast('Somebody created a new keyboard!', {id: JSON.stringify(keyboard)});
+        }
+        await getKeyboards();
+      })
+
+      keyboardsContract.on('TipSent', (recipient, amount) => {
+        if (addressesEqual(recipient, connectedAccount)) {
+          toast(`You received a tip of ${ethers.utils.formatEther(amount)} eth!`, {id: recipient + amount});
+        }
+      })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [!!keyboardsContract, connectedAccount]);
+
+  if (!ethereum) {
+    return <p>Please install MetaMask to connect to this site</p>
+  }
+
+  if (!connectedAccount) {
+    return <PrimaryButton onClick={connectAccount}>Connect MetaMask Wallet</PrimaryButton>
+  }
+
+  if (keyboardsLoading) {
+    return (
+      <div className="flex flex-col gap-4">
+        <PrimaryButton type="link" href="/create">Create a Keyboard!</PrimaryButton>
+        <p>Loading Keyboards...</p>
+      </div>
+    )
+  }
+
+  if (keyboards.length > 0) {
+    return (
+      <div className="flex flex-col gap-4">
+        <PrimaryButton type="link" href="/create">Create a Keyboard!</PrimaryButton>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 p-2">
+          {keyboards.map(
+            ([kind, isPBT, filter, owner], i) => (
+              <div key={i} className="relative">
+                <Keyboard kind={kind} isPBT={isPBT} filter={filter} />
+                <span className="absolute top-1 right-6">
+                  {addressesEqual(owner, connectedAccount) ?
+                    <UserCircleIcon className="h-5 w-5 text-indigo-100" /> :
+                    <TipButton keyboardsContract={keyboardsContract} index={i} />
+                  }
+                </span>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <PrimaryButton type="link" href="/create">Create a Keyboard!</PrimaryButton>
+      <p>No keyboards yet!</p>
+    </div>
+  )
 }
